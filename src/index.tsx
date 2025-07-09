255e1616636779a8d92b1879a143f7db9e1ace81
Alexey Ushakov <develengie@gmail.com>
2025-07-09 13:15:34 +0300

    chore: removed Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./store";
 import "./index.scss";
 import { MainApp } from "./apps/MainApp";
 import reportWebVitals from "./reportWebVitals";
@@ -11,9 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <MainApp />
-        </Provider>
+        <MainApp />
     </React.StrictMode>
 );
 
